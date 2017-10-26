0d25c4e509b966323e4cb5d4223475dc5271a3d8
Kyle Smith <kyle.smithy2@gmail.com>
2017-10-26 19:50:07 +1000

    DatagramIterator: Added base class
    - No logic implemented yet

---
+++ datagram/DatagramIterator.py
@@ -0,0 +1,75 @@
+import struct
+
+'''
+The datagram iterator file responsible for retrieving values out of datagram's.
+This code was largely ported from Panda3D's C++ implementation of Datagram's.
+'''
+
+class DatagramIterator:
+	
+	# Core functions surrounding datagrams
+	def __init__(self, datagram):
+		self.datagram = datagram
+
+	def getBool(self):
+		pass
+
+	def getInt8(self):
+		pass
+
+	def getInt16(self):
+		pass
+
+	def getInt32(self):
+		pass
+
+	def getInt64(self):
+		pass
+
+	def getString(self):
+		pass
+
+	def getString32(self):
+		pass
+
+	def getUint8(self):
+		pass
+
+	def getUint16(self):
+		pass
+
+	def getUint32(self):
+		pass
+
+	def getUint64(self):
+		pass
+
+	def getFloat32(self):
+		pass
+
+	def getFloat64(self):
+		pass
+
+	def getBeInt16(self):
+		pass
+
+	def getBeInt32(self):
+		pass
+
+	def getBeInt64(self):
+		pass
+
+	def getBeUint16(self):
+		pass
+
+	def getBeUint32(self):
+		pass
+
+	def getBeUint64(self):
+		pass
+
+	def getBeFloat32(self):
+		pass
+
+	def getBeFloat64(self):
+		pass
